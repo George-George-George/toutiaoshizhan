235cf6186057255ed29b31f5a8a96ae4de6045b6
qiaozhi <1326160951@qq.com>
2022-07-13 12:07:04 +0800

    配置了包

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
   <div>
-    <h1>黑马头条</h1>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <div class="box1"></div>
   </div>
 </template>
 <script>
 export default {}
 </script>
-<style lang="scss" scoped></style>
+<style lang="less" scoped>
+.box1 {
+  width: 750px;
+  height: 88px;
+  background-color: #6db4fb;
+  border-radius: 5px;
+}
+</style>
